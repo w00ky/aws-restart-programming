653fb25046f06011096346617d06c5263a1848af
w00ky <jinwooksukbiz@gmail.com>
2023-08-10 10:09:04 +1000

    aws restart

---
+++ data_types.py
@@ -0,0 +1,12 @@
+1 # Integer - it is a whole number
+2.0 # Float - a number with a decimal point
+True # Boolean
+False # Boolean - true or false statement
+"Hello World" # String - any character that is enclosed by quotation marks
+'900' # String - because it is enclosed by quotation marks
+
+[]  # List - A collection of characters enclosed by square brackets, it is immutable
+()  # Tuple - Immutable
+{}  # Dictionary - a mutable collection of key-value pairs enclosed by curly braces.
+    # Each key and value is linked via a colon and each pair is separated by commas
+
